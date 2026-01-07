0725cbd8a090394093ccc4ffe6bd8f4f77f16fe0
jaowens14 <jaowens14@protonmail.com>
2026-01-07 05:34:42 -0600

    snmp dbus prototype

---
+++ snmp_client.py
@@ -0,0 +1,36 @@
+from pprint import pprint
+from dbus_next.aio import MessageBus
+
+import asyncio
+
+loop = asyncio.get_event_loop()
+from dbus_next.constants import BusType
+
+async def main():
+    bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
+    # the introspection xml would normally be included in your project, but
+    # this is convenient for development
+    introspection = await bus.introspect('test.name', '/test/path')
+
+    obj = bus.get_proxy_object('test.name', '/test/path', introspection)
+    interface = obj.get_interface('test.interface')
+    #properties = obj.get_interface('org.freedesktop.DBus.Properties')
+
+    ## call methods on the interface (this causes the media player to play)
+    resp = await interface.call_get_v2_users()
+    pprint(resp)
+    #volume = await player.get_volume()
+    #print(f'current volume: {volume}, setting to 0.5')
+#
+    #await player.set_volume(0.5)
+#
+    ## listen to signals
+    #def on_properties_changed(interface_name, changed_properties, invalidated_properties):
+    #    for changed, variant in changed_properties.items():
+    #        print(f'property changed: {changed} - {variant.value}')
+#
+    #properties.on_properties_changed(on_properties_changed)
+
+    await loop.create_future()
+
+loop.run_until_complete(main())
